85b940e550f271954a470d39cecfde41e561d796
Caerod1980 <caerod@gmail.com>
2026-05-16 18:16:54 -0300

    Add CHANNELS constant and CHANNELS_LIST export

---
+++ backend/src/constants/channels.js
@@ -0,0 +1,13 @@
+const CHANNELS = {
+  LOJA_FISICA: 'loja_fisica',
+  BEBCOM_DELIVERY: 'bebcom_delivery',
+  IFOOD: 'ifood',
+  WHATSAPP: 'whatsapp',
+  BEBCOM_LOUNGE: 'bebcom_lounge',
+  EVENTOS: 'eventos',
+  OUTROS: 'outros',
+};
+
+const CHANNELS_LIST = Object.values(CHANNELS);
+
+module.exports = { CHANNELS, CHANNELS_LIST };
